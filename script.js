3df69822c3f52eb6aa8bdb6d43297d73cb059672
Logan McKee <Loganmckeenewpaypal@gmail.com>
2024-12-29 14:50:03 +0000

    Added modal dialogue containing form for submitting new book.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const myLibrary = [];
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("button");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", (e) => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener("click", (e) => {
+    dialog.close();
+});
 
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -13,13 +24,21 @@ function addBookToLibrary(title, author, pages, isRead) {
 }
 
 function listBooks(books) {
+    const tableBody = document.getElementById("table-body");
     for (book of books) {
-        console.log(book);
+        let newRow = tableBody.insertRow(0);
+        newRow.insertCell(0).textContent = book.title;
+        newRow.insertCell(1).textContent = book.author;
+        newRow.insertCell(2).textContent = book.pages;
+        let checkboxRow = newRow.insertCell(3);
+        let isReadCheckbox = document.createElement("input");
+        isReadCheckbox.type = "checkbox";
+        isReadCheckbox.checked = book.isRead;
+        checkboxRow.appendChild(isReadCheckbox);
     }
 }
 
-
-addBookToLibrary("Testbook", "Some author", 231, true);
+addBookToLibrary("Testbook", "Some author", 231, true); //use array here!
 addBookToLibrary("Testbook2", "Some author2", 331, false);
 addBookToLibrary("Testbook3", "Some author3", 3321, false);
 listBooks(myLibrary);
