cc11c33d95d6e2700ea8dd1786b34c4b42f57242
Logan McKee <Loganmckeenewpaypal@gmail.com>
2024-12-29 16:33:23 +0000

    Added functionality to support user created books via modal dialog form, and append these to table.

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ const myLibrary = [];
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("button");
 const closeButton = document.querySelector("dialog button");
+const newBookForm = document.querySelector("dialog > form");
 
 showButton.addEventListener("click", (e) => {
     dialog.showModal();
 });
 
 closeButton.addEventListener("click", (e) => {
+    dialog.close();
+});
+
+newBookForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const isRead = document.getElementById("is-read").checked;
+    let book = addBookToLibrary(title, author, pages, isRead);
+    appendBook(book);
+    newBookForm.reset();
     dialog.close();
 });
 
@@ -21,25 +34,30 @@ function Book(title, author, pages, isRead) {
 function addBookToLibrary(title, author, pages, isRead) {
     let book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
+    return book;
 }
 
-function listBooks(books) {
+function appendBook(book) {
     const tableBody = document.getElementById("table-body");
+    let newRow = tableBody.insertRow(0);
+    newRow.insertCell(0).textContent = book.title;
+    newRow.insertCell(1).textContent = book.author;
+    newRow.insertCell(2).textContent = book.pages;
+    let checkboxRow = newRow.insertCell(3);
+    let isReadCheckbox = document.createElement("input");
+    isReadCheckbox.type = "checkbox";
+    isReadCheckbox.checked = book.isRead;
+    checkboxRow.appendChild(isReadCheckbox);
+}
+
+function displayLibrary(books) {
     for (book of books) {
-        let newRow = tableBody.insertRow(0);
-        newRow.insertCell(0).textContent = book.title;
-        newRow.insertCell(1).textContent = book.author;
-        newRow.insertCell(2).textContent = book.pages;
-        let checkboxRow = newRow.insertCell(3);
-        let isReadCheckbox = document.createElement("input");
-        isReadCheckbox.type = "checkbox";
-        isReadCheckbox.checked = book.isRead;
-        checkboxRow.appendChild(isReadCheckbox);
+        appendBook(book);
     }
 }
 
 addBookToLibrary("Testbook", "Some author", 231, true); //use array here!
 addBookToLibrary("Testbook2", "Some author2", 331, false);
 addBookToLibrary("Testbook3", "Some author3", 3321, false);
-listBooks(myLibrary);
+displayLibrary(myLibrary);
 
