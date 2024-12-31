69cb219a7a7af8345771d669d55f1e7cbd048df7
Logan McKee <Loganmckeenewpaypal@gmail.com>
2024-12-31 13:06:11 +0000

    Refactored constructors to IIFEs/factory functions, practicing closures

--- script.js
+++ script.js
@@ -1,79 +1,99 @@
-const myLibrary = [];
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("button[title='Add book']");
-const closeButton = document.querySelector("dialog button");
-const BookForm = document.querySelector("dialog > form");
+const myLibrary = (() => { //IIFE Factory Function for encapsulation
+    const books = [];
 
-showButton.addEventListener("click", (e) => {
-    dialog.showModal();
-});
+    function createBook(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+        return { title, author, pages, isRead }
+    };
 
-closeButton.addEventListener("click", (e) => {
-    dialog.close();
-    BookForm.reset();
-});
+    function addBookToLibrary(title, author, pages, isRead) {
+        let book = createBook(title, author, pages, isRead);
+        books.push(book);
+        return book;
+    };
 
-BookForm.addEventListener("submit", e => {
-    e.preventDefault();
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-    const isRead = document.getElementById("is-read").checked;
-    let book = addBookToLibrary(title, author, pages, isRead);
-    appendBook(book);
-    BookForm.reset();
-    dialog.close();
-});
-
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-}
-
-Book.prototype.deleteRow = function (button) {
-    let row = button.parentNode.parentNode;
-    let table = document.getElementById("book-table");
-    table.deleteRow(row.rowIndex);
-};
-
-function addBookToLibrary(title, author, pages, isRead) {
-    let book = new Book(title, author, pages, isRead);
-    myLibrary.push(book);
-    return book;
-}
-
-function appendBook(book) {
-    const tableBody = document.getElementById("table-body");
-    let newRow = tableBody.insertRow(0);
-    newRow.insertCell(0).textContent = book.title;
-    newRow.insertCell(1).textContent = book.author;
-    newRow.insertCell(2).textContent = book.pages;
-    let checkboxRow = newRow.insertCell(3);
-    let isReadCheckbox = document.createElement("input");
-    isReadCheckbox.type = "checkbox";
-    isReadCheckbox.checked = book.isRead;
-    checkboxRow.appendChild(isReadCheckbox);
-    let deleteButton = document.createElement("button");
-    deleteButton.classList.add("delete-button");
-    deleteButton.setAttribute("onclick", "book.deleteRow(this)");
-    deleteButton.innerHTML = "Delete book";
-    let deleteRow = newRow.insertCell(4);
-    deleteRow.appendChild(deleteButton);
-};
-
-
-function displayLibrary(books) {
-    for (book of books) {
-        appendBook(book);
+    function returnBooks() {
+        return books;
     }
-}
 
-function removeBook(index) {
-    myLibrary.splice(index, 1);
-}
+    return { createBook, addBookToLibrary, returnBooks };
+})();
 
-addBookToLibrary("Testbook", "Some author", 231, true);
-displayLibrary(myLibrary);
+const bookTable = (() => { //Another IIFE
+    function cacheDom() {
+        this.showFormButton = document.querySelector("button[title='Add book'");
+        this.bookForm = document.querySelector("form");
+        this.tableBody = document.getElementById("table-body");
+        this.closeFormButton = document.querySelector("button[type='button']");
+        this.modalDialog = document.querySelector("dialog");
+        return this;
+    }
+
+
+    function addTableRow(book) {
+        let newRow = cacheDom().tableBody.insertRow(0);
+        newRow.insertCell(0).textContent = book.title;
+        newRow.insertCell(1).textContent = book.author;
+        newRow.insertCell(2).textContent = book.pages;
+        let checkboxRow = newRow.insertCell(3);
+        let isReadCheckbox = document.createElement("input");
+        isReadCheckbox.type = "checkbox";
+        isReadCheckbox.checked = book.isRead;
+        checkboxRow.appendChild(isReadCheckbox);
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-button");
+        deleteButton.setAttribute("onclick", "bookTable.deleteRow(this)");
+        deleteButton.innerHTML = "Delete book";
+        let deleteRow = newRow.insertCell(4);
+        deleteRow.appendChild(deleteButton);
+    };
+
+    function buildTable(books) {
+        for (book of books) {
+            addTableRow(book);
+        }
+    };
+
+    function submitUserCreatedBook() {
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pages = document.getElementById("pages").value;
+        const isRead = document.getElementById("is-read").checked;
+        let book = myLibrary.addBookToLibrary(title, author, pages, isRead);
+        addTableRow(book);
+        bookForm.reset();
+        modalDialog.close();
+    };
+
+    function deleteRow(button) {
+        let row = button.parentNode.parentNode;
+        let table = document.getElementById("book-table");
+        table.deleteRow(row.rowIndex);
+    };
+    return { cacheDom, addTableRow, buildTable, submitUserCreatedBook, deleteRow }
+})();
+
+
+const libraryTable = bookTable.cacheDom();
+
+libraryTable.showFormButton.addEventListener("click", () => {
+    libraryTable.modalDialog.showModal();
+});
+
+libraryTable.closeFormButton.addEventListener("click", () => {
+    libraryTable.modalDialog.close();
+    libraryTable.bookForm.reset();
+});
+
+libraryTable.bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    libraryTable.submitUserCreatedBook();
+});
+
+const newBook = myLibrary.addBookToLibrary("A Test Book", "Some Dude", "200", "True");
+libraryTable.buildTable(myLibrary.returnBooks());
+
 
