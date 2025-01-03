26fcd25b0465a2c10599bc328fcb70a0bacb58bc
Logan McKee <Logan.Mckee@version1.com>
2025-01-03 13:28:57 +0000

    Refactored to use classes instead of constructors.

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-const myLibrary = (() => { //IIFE Factory Function for encapsulation
-    const books = [];
-
-    function createBook(title, author, pages, isRead) {
+class Book {
+    constructor(title, author, pages, isRead) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.isRead = isRead;
-        return { title, author, pages, isRead }
-    };
+    }
+};
 
-    function addBookToLibrary(title, author, pages, isRead) {
-        let book = createBook(title, author, pages, isRead);
-        books.push(book);
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBookToLibrary(title, author, pages, isRead) {
+        let book = new Book(title, author, pages, isRead);
+        this.books.push(book);
         return book;
     };
 
-    function returnBooks() {
-        return books;
+    getBooks() {
+        return this.books;
+    }
+};
+
+class UIController {
+    constructor() {
+        this.cacheDom(); //Runs cacheDOM on construct
     }
 
-    return { createBook, addBookToLibrary, returnBooks };
-})();
-
-const bookTable = (() => { //Another IIFE
-    function cacheDom() {
-        this.showFormButton = document.querySelector("button[title='Add book'");
+    cacheDom() { //Utility function to cache DOM elements
+        this.showFormButton = document.querySelector("button[title='Add book']");
         this.bookForm = document.querySelector("form");
         this.tableBody = document.getElementById("table-body");
         this.closeFormButton = document.querySelector("button[type='button']");
         this.modalDialog = document.querySelector("dialog");
-        return this;
-    }
+        this.bookTable = document.getElementById("book-table");
+    };
 
-
-    function addTableRow(book) {
-        let newRow = cacheDom().tableBody.insertRow(0);
+    addTableRow(book) {
+        let newRow = this.tableBody.insertRow(0);
         newRow.insertCell(0).textContent = book.title;
         newRow.insertCell(1).textContent = book.author;
         newRow.insertCell(2).textContent = book.pages;
@@ -45,42 +49,43 @@ const bookTable = (() => { //Another IIFE
         checkboxRow.appendChild(isReadCheckbox);
         let deleteButton = document.createElement("button");
         deleteButton.classList.add("delete-button");
-        deleteButton.setAttribute("onclick", "bookTable.deleteRow(this)");
-        deleteButton.innerHTML = "Delete book";
+        deleteButton.setAttribute("onclick", "uiController.deleteRow(this)");
+        deleteButton.textContent = "Delete book";
         let deleteRow = newRow.insertCell(4);
         deleteRow.appendChild(deleteButton);
     };
 
-    function buildTable(books) {
-        for (book of books) {
-            addTableRow(book);
+    buildTable(books) {
+        for (const book of books) {
+            this.addTableRow(book);
         }
     };
 
-    function submitUserCreatedBook() {
+    submitUserCreatedBook() {
         const title = document.getElementById("title").value;
         const author = document.getElementById("author").value;
         const pages = document.getElementById("pages").value;
         const isRead = document.getElementById("is-read").checked;
-        let book = myLibrary.addBookToLibrary(title, author, pages, isRead);
-        addTableRow(book);
-        bookForm.reset();
-        modalDialog.close();
+        const book = myLibrary.addBookToLibrary(title, author, pages, isRead);
+        this.addTableRow(book);
+        this.bookForm.reset();
+        this.modalDialog.close();
     };
 
-    function deleteRow(button) {
+    deleteRow(button) {
         let row = button.parentNode.parentNode;
-        let table = document.getElementById("book-table");
-        table.deleteRow(row.rowIndex);
+        this.bookTable.deleteRow(row.rowIndex);
     };
-    return { cacheDom, addTableRow, buildTable, submitUserCreatedBook, deleteRow }
-})();
+};
 
-(() => { // Encapsulating bottom lines in an IIFE for better scope management 
-    bookTable.cacheDom();
-    bookTable.showFormButton.addEventListener("click", () => { bookTable.modalDialog.showModal(); });
-    bookTable.closeFormButton.addEventListener("click", () => { bookTable.modalDialog.close(); bookTable.bookForm.reset(); });
-    bookTable.bookForm.addEventListener("submit", (e) => { e.preventDefault(); bookTable.submitUserCreatedBook(); });
-    myLibrary.addBookToLibrary("A Test Book", "Some Dude", "200", "True"); bookTable.buildTable(myLibrary.returnBooks());
+const uiController = new UIController();
+const myLibrary = new Library();
+
+(() => {
+    uiController.showFormButton.addEventListener("click", () => { uiController.modalDialog.showModal(); });
+    uiController.closeFormButton.addEventListener("click", () => { uiController.modalDialog.close(); uiController.bookForm.reset(); });
+    uiController.bookForm.addEventListener("submit", (e) => { e.preventDefault(); uiController.submitUserCreatedBook(); });
+    myLibrary.addBookToLibrary("A Test Book", "Some Dude", "200", "True");
+    uiController.buildTable(myLibrary.getBooks());
 })();
 
